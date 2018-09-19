8e07f8a618e12e703d47de4a3add5eeea25807b7
yangzl <youngzil@163.com>
2018-09-19 14:07:36 +0800

    新增spring-boot-web示例 跨域解决代码CorsConfig

---
+++ quickstart-spring-boot-web-example/src/main/java/org/quickstart/spring/boot/web/example/Application.java
@@ -0,0 +1,35 @@
+/**
+ * 项目名称：quickstart-spring-boot 
+ * 文件名：App.java
+ * 版本信息：
+ * 日期：2017年5月7日
+ * Copyright asiainfo Corporation 2017
+ * 版权所有 *
+ */
+package org.quickstart.spring.boot.web.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * App
+ * 
+ * @author：yangzl@asiainfo.com
+ * @2017年5月7日 下午12:14:46
+ * @version 1.0
+ */
+@SpringBootApplication
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    // http://localhost:8081/hello/sayHello
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+        logger.info("My Spring Boot Application Started");
+    }
+
+}
---
+++ quickstart-spring-boot-web-example/src/main/java/org/quickstart/spring/boot/web/example/controller/WebController.java
@@ -0,0 +1,35 @@
+package org.quickstart.spring.boot.web.example.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/web")
+public class WebController {
+
+    @RequestMapping("/hi")
+    public String hi(Map<String, Object> model) {
+        model.put("message", "这是测试的内容。。。");
+        model.put("toUserName", "张三");
+        model.put("fromUserName", "老许");
+        return "welcome"; // 自动寻找resources/templates中名字为welcome.ftl/welcome.vm的文件作为模板，拼装后返回
+    }
+
+    // @InitBinder
+    // public void initBinder(WebDataBinder binder) {
+    // SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    // dateFormat.setLenient(false);
+    // binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true)); //true:允许输入空值，false:不能
+    // }
+
+    // @InitBinder
+    // protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) {
+    // SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    // simpleDateFormat.setLenient(false);
+    // CustomDateEditor dateEditor = new CustomDateEditor(simpleDateFormat, true);
+    // binder.registerCustomEditor(Date.class,dateEditor);
+    // }
+
+}
