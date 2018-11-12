68f6c851015553256c8957f93eb1955528ca0c39
yangzl <youngzil@163.com>
2018-11-12 15:59:25 +0800

    新增jooq示例

---
+++ quickstart-spring-boot-jooq/src/main/java/org/quickstart/spring/boot/jooq/Application.java
@@ -0,0 +1,16 @@
+package org.quickstart.spring.boot.jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Thomas Darimont
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
